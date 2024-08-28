e44c0c9577dcc3785e87c3e01071b209c27874d3
naeemul-online <naeemul.online@gmail.com>
2024-08-28 20:21:16 +0300

    product card section fixed

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -3,7 +3,7 @@
 const ProductCard = ({ product }) => {
   const { name, description, category, createdAt, price, ratings, image } =
     product;
-  // console.log(typeof createdAt);
+
   return (
     <div className=" flex flex-col justify-between bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 hover:shadow-xl">
       <a href="#">
@@ -79,13 +79,7 @@ const ProductCard = ({ product }) => {
         <div className="flex items-center justify-between">
           <span className="text-3xl font-bold text-gray-900 dark:text-white">
             ${price}
-          </span>
-          {/* <a
-            href="#"
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Buy Now
-          </a> */}
+          </span>        
         </div>
       </div>
     </div>
